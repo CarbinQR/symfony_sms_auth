1be1e8b62cb5959ee9d9a6d72fce5e6dccfd714e
m.suslov <m.suslov@softsvit.com>
2025-02-17 12:39:42 +0200

    refactoring

--- app/src/Controller/AuthController.php
+++ app/src/Controller/AuthController.php
@@ -5,22 +5,20 @@ namespace App\Controller;
 use App\Request\Auth\VerifyAuthCodeRequest;
 use App\Service\User\AuthService;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AuthController extends ApiController
 {
     #[Route('/api/auth/verify', name: 'app_auth_verify_code', defaults: ['_rate_limited' => false], methods: 'POST')]
     public function verifyCode(
-        Request               $request,
         AuthService           $authService,
-        VerifyAuthCodeRequest $authCodeRequest
+        VerifyAuthCodeRequest $request
     ): JsonResponse
     {
         return $this->success(
             $authService->verifyCode(
                 json_decode(
-                    $request->getContent(), true, 512, JSON_THROW_ON_ERROR
+                    $request->getRequest()->getContent(), true, 512, JSON_THROW_ON_ERROR
                 )
             )
         );
--- app/src/Controller/SmsController.php
+++ app/src/Controller/SmsController.php
@@ -5,22 +5,20 @@ namespace App\Controller;
 use App\Request\Sms\SendAuthCodeRequest;
 use App\Service\Sms\SmsService;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SmsController extends ApiController
 {
     #[Route('/api/sms/send/auth', name: 'app_sms_send_auth', defaults: ['_rate_limited' => true], methods: 'POST')]
     public function sendAuthCode(
-        Request    $request,
         SmsService $smsService,
-        SendAuthCodeRequest $authCodeRequest
+        SendAuthCodeRequest $request
     ): JsonResponse
     {
         return $this->success(
             $smsService->sendAuthCode(
                 json_decode(
-                    $request->getContent(), true, 512, JSON_THROW_ON_ERROR
+                    $request->getRequest()->getContent(), true, 512, JSON_THROW_ON_ERROR
                 )
             )
         );
